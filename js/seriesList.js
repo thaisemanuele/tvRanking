5adb16d6e53921cb84c05c97d460ba698f313bbb
thatitis <thatitis@gmail.com>
2015-05-14 23:18:44 -0300

    reading from json

---
+++ js/seriesList.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $.getJSON("js/json/acao.json", function (data) {
+        alert(data[0].acao[0].title);
+        for (var i = 0; i <data[0].acao.length; i++) {
+            var rating = countStars(data[0].acao[i].rating);
+            $(".main-holder").append('<article class="col-md-10 col-md-offset-1"><img class="show-img col-sm-5 img-responsive" src="img/posters/' + data[0].acao[i].image + '" alt=""><div class="show-info col-sm-8  centralize"><h1 class="show-title">' +
+                data[0].acao[i].title + '</h1><span class="col-sm-11 col-sm-offset-1 rating-stars">'+ rating+'                    </span><ul class="more-info"><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].acao[i].year + '</li><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].acao[i].duration + '</li><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].acao[i].channel + '</li><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].acao[i].status + '</li></ul><div class="description"><p>' + data[0].acao[i].description + '</p><p>Avaliado por <span class="users_number">X </span>usuários</p></div><a href="#" class="compare col-sm-10 col-sm-offset-1">Compare</a></div>         </article>');
+        } /*End For*/
+
+    });
+
+    function countStars(rating) {
+        var stars = "";
+        for (var i = 1; i < 6; i++) {
+            if (rating >= i) {
+                stars = stars + '<img src="img/rating_star.png" alt="uma estrela">';
+            } else if (rating > i - 1) {
+                stars = stars + '<img src="img/half_star.png" alt="meia estrela">';
+            } else {
+                stars = stars + '<img src="img/rating_star_gray.png" alt="zero estrela">';
+            }
+        }
+        return stars;
+    }
+});
