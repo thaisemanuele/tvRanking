375a4f21c654e57c0ab9aca0a7e0a5c1acc6304c
thatitis <thatitis@gmail.com>
2015-05-22 15:08:59 -0300

    inseridas serie em crime

--- js/crimeList.js
+++ js/crimeList.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $.getJSON("js/json/crime.json", function (data) {
-       for (var i = 0; i <data[0].comedia.length; i++) {
-            var rating = countStars(data[0].comedia[i].rating);
-            $(".main-holder").append('<article class="col-md-10 col-md-offset-1"><img class="show-img col-sm-5 img-responsive" src="img/posters/' + data[0].comedia[i].image + '" alt=""><div class="show-info col-sm-8  centralize"><h1 class="show-title">' +
-                data[0].comedia[i].title + '</h1><span class="col-sm-11 col-sm-offset-1 rating-stars">'+ rating+'                    </span><ul class="more-info"><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].comedia[i].year + '</li><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].comedia[i].duration + '</li><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].comedia[i].channel + '</li><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].comedia[i].status + '</li></ul><div class="description"><p>' + data[0].comedia[i].description + '</p><p>Avaliado por <span class="users_number">X </span>usuários</p></div><a href="#" class="compare col-sm-10 col-sm-offset-1">Compare</a></div>         </article>');
+       for (var i = 0; i <data[0].crime.length; i++) {
+            var rating = countStars(data[0].crime[i].rating);
+            $(".main-holder").append('<article class="col-md-10 col-md-offset-1"><img class="show-img col-sm-5 img-responsive" src="img/posters/' + data[0].crime[i].image + '" alt=""><div class="show-info col-sm-8  centralize"><h1 class="show-title">' +
+                data[0].crime[i].title + '</h1><span class="col-sm-11 col-sm-offset-1 rating-stars">'+ rating+'                    </span><ul class="more-info"><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].crime[i].year + '</li><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].crime[i].duration + '</li><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].crime[i].channel + '</li><li><span class="glyphicon glyphicon-star col-sm-1" aria-hidden="true"></span>' + data[0].crime[i].status + '</li></ul><div class="description"><p>' + data[0].crime[i].description + '</p><p>Avaliado por <span class="users_number">X </span>usuários</p></div><a href="#" class="compare col-sm-10 col-sm-offset-1">Compare</a></div>         </article>');
         } /*End For*/
 
     });
